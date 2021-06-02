f9bbd1390fc4f1156d18f804f9056d20fd915829
Jordan Jordanov <dcooldgames@gmail.com>
2021-06-02 15:19:45 +0300

    Update footer.php

--- public_html/footer.php
+++ public_html/footer.php
@@ -4,7 +4,7 @@
 					<ul class="list icons">
 						<li class="item"> <a href="https://www.instagram.com/"><i class="flaticon-instagram"></i>instagram</a></li>
 						<li class="item"><a href="https://linkedin.com/"><i class="flaticon-linkedin"></i>linkedin</a></li>
-						<li class="item"><a href="https://www.goo.gl/"><i class="flaticon-place"></i>place</a></li>
+						<li class="item"><a href="https://www.google.com/maps"><i class="flaticon-place"></i>place</a></li>
 					</ul>				
 				</section>					
 			</div>
